a883ed3c1c1a5d877480775382a9d9c6e47effb7
jsimmons--dev <jsimmons@udel.edu>
2016-03-14 17:18:21 -0400

    added pivotJS to the library and a quick example (in src/main.js)

--- app/built-script.js
+++ app/built-script.js
@@ -2,29 +2,42 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Test = function Test() {
-  _classCallCheck(this, Test);
+    _classCallCheck(this, Test);
 };
 
 ;
 var a = 1,
     b = 2,
     foo = {
-  a: a, b: b
+    a: a, b: b
 },
     sum = function sum() {
-  return a + b;
+    return a + b;
 };
 
 console.log(sum());
 console.log("I'm syncing ES6!");
 
 exports["default"] = a;
+
+var data = [["row", "total_bill", "tip", "sex", "smoker", "day", "time", "size"], ["1", 16.99, 1.01, "Female", "No", "Sun", "Dinner", 2], ["2", 10.34, 1.66, "Male", "No", "Sun", "Dinner", 3], ["3", 21.01, 3.5, "Male", "No", "Sun", "Dinner", 3], ["4", 23.68, 3.31, "Male", "No", "Sun", "Dinner", 2]];
+
+var utils = $.pivotUtilities;
+var heatmap = utils.renderers["Heatmap"];
+var sumOverSum = utils.aggregators["Sum over Sum"];
+
+$("#output").pivotUI(data, {
+    rows: ["sex", "smoker"],
+    cols: ["day", "time"],
+    aggregator: sumOverSum(["tip", "total_bill"]),
+    renderer: heatmap
+});
 module.exports = exports["default"];
 
 },{}]},{},[1]);
--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-class Test {};
-let a = 1,
-    b = 2,
-    foo = {
-        a, b
-    },
-    sum = () => {
-        return a + b;
-    };
+var data = [
+	["row","total_bill","tip","sex","smoker","day","time","size"],
+	["1",16.99,1.01,"Female","No","Sun","Dinner",2],
+	["2",10.34,1.66,"Male","No","Sun","Dinner",3],
+	["3",21.01,3.5,"Male","No","Sun","Dinner",3],
+	["4",23.68,3.31,"Male","No","Sun","Dinner",2],
+];
 
-console.log(sum());
-console.log("I'm syncing ES6!");
+var utils = $.pivotUtilities;
+var heatmap =  utils.renderers["Heatmap"];
+var sumOverSum =  utils.aggregators["Sum over Sum"];
 
-export default a;
+$("#output").pivotUI(
+  data, {
+    rows: ["sex", "smoker"],
+    cols: ["day", "time"],
+    aggregator: sumOverSum(["tip", "total_bill"]),
+    renderer: heatmap
+  });
