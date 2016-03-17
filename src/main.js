7aee515863cb6dfa9111aa030ffe46ef498f41b6
jsimmons--dev <jsimmons@udel.edu>
2016-03-16 21:22:28 -0400

    updated index to have more content; main now has relavant data

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 var data = [
-	["row","total_bill","tip","sex","smoker","day","time","size"],
-	["1",16.99,1.01,"Female","No","Sun","Dinner",2],
-	["2",10.34,1.66,"Male","No","Sun","Dinner",3],
-	["3",21.01,3.5,"Male","No","Sun","Dinner",3],
-	["4",23.68,3.31,"Male","No","Sun","Dinner",2],
-];
+     [ "dateTime", "value"],
+	 ["2015-02-23", 2188],
+     ["2015-02-24", 2744],
+     ["2015-02-25", 2162],
+     ["2015-02-26", 2818],
+     ["2015-02-27", 2163],
+     ["2015-02-28", 2274],
+     ["2015-03-01", 2269]
+  ];
 
-var utils = $.pivotUtilities;
-var heatmap =  utils.renderers["Heatmap"];
-var sumOverSum =  utils.aggregators["Sum over Sum"];
-
-$("#output").pivotUI(
+$("#pivot").pivotUI(
   data, {
-    rows: ["sex", "smoker"],
-    cols: ["day", "time"],
-    aggregator: sumOverSum(["tip", "total_bill"]),
-    renderer: heatmap
   });
