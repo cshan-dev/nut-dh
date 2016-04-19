0070b0bda1a8edd16570c99a8c199caf10311211
mbamundo <mbamundo@udel.edu>
2016-04-19 13:17:24 -0400

    authenticate 1 user at a time, get tokens for user, get step data from user and print on screen

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
             },
             js: {
                 files: ['src/**/*.js'],
-                tasks: ['browserify', 'bsReload:js', 'karma', 'eslint']
+                tasks: ['browserify', 'bsReload:js']
             },
             test: {
                 files: ['test/**/*.tests.js'],
