7cff0feb01eda04e60b0e9240497981637b29600
Krizman <eric.krizman@gmail.com>
2016-05-12 22:33:08 -0400

    mocha tests for server.js in backend

--- test/backend/server.test.js
+++ test/backend/server.test.js
@@ -11,3 +11,30 @@ describe('/', function() {
 	});
 });
 
+describe('loop Url', function(){
+	it('shoudnt be null', function(){
+		var result = server.loopUrls(0, new Date('2016-05-12'), new Date('2016-05-13'), '1min', null);
+		assert(result !== null);
+	});
+});
+
+describe('format date', function(){
+	it('shoudnt be null', function(){
+		var result = server.formatDate(new Date('2016-05-12'));
+		assert(result !== null);
+	});
+});
+
+describe('data transform', function(){
+	it('shoudnt be null', function(){
+		var result = server.dataTransform('2016-05-12');
+		assert(result !== null);
+	});
+});
+
+describe('get activity data', function(){
+	it('shoudnt be null', function(){
+		var result = server.getActivityData('activities/calories', new Date('2016-05-12'), new Date('2016-05-13'), '1min');
+		assert(result !== null);
+	});
+});
