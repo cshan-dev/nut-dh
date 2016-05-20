08d2c7baf26c0e40f77b714f66201d1227d11937
jsimmons--dev <jsimmons@udel.edu>
2016-05-20 01:09:18 -0400

    patched multiusers over multiple days

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ var clientEncoded = "MjI3SDhEOmJmODg0ZmExMjBmMzE0MjE2OGEwOTgyNTdlMzRlYTEz";
 var accessToken = "";
 var refreshToken = "";
 var username = "";
-//var urls = ["activities/calories", "activities/distance", "activities/minutesSedentary", "activities/minutesLightlyActive", "activities/minutesFairlyActive", "activities/minutesVeryActive", "activities/heart", "activities/steps"];
-var urls = ["activities/calories", "activities/distance"]
+var urls = ["activities/calories", "activities/distance", "activities/minutesSedentary", "activities/minutesLightlyActive", "activities/minutesFairlyActive", "activities/minutesVeryActive", "activities/heart", "activities/steps"];
+//var urls = ["activities/calories"]
 let dbURL = 'mongodb://localhost:27017/tokens';
 const PORT = 22205;
 
@@ -28,48 +28,28 @@ router.use(bodyParser.urlencoded({
 
 router.use(express.static(__dirname + '/../app'));
 
-router.get('/index', function(req, res) {
-	res.sendFile('index.html', { root : path.join(__dirname, '../app')});
-
-});
-
-router.get('/admin', function(req, res) {
-	res.sendFile('index.html', { root : path.join(__dirname, '../app/login')});
-
-})
-
 function getUTC(date) {
     var date_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
     return date_utc;
 }
 router.get('/getAllData/:begin/:end/:interval', (req, res) => {
-    var start = req.params.begin;
-    var end = req.params.end;
-    let originalStart = new Date(start);
-    originalStart.setDate(originalStart.getDate() + 1)
-    originalStart.setHours(0);
+    let start = new Date(req.params.begin).getTime();
+    let end = new Date(req.params.end).getTime();
+	console.log("Start string:",req.params.begin,"start",start,"End String",req.params.end,"end",end);
+    let intervalString = req.params.interval;
+	console.log("intervalString",intervalString);
+    let interval = +intervalString.substring(0, intervalString.length - 3);
 
-    let originalEnd = new Date(end);
-    originalEnd.setDate(originalEnd.getDate() + 1)
-    originalEnd.setHours(0);
-    var interval = req.params.interval;
-
-    let datesbtn = 1 + ((originalEnd.getTime() - originalStart.getTime()) / 86400000);
+    let datesbtn = 1 + ((end - start) / 86400000);
+	console.log("dates between",datesbtn);
 
     let fulfilled = 0;
-    let max = 0;
-
 
     let resultArray = [];
 
-    user.getUsers({}, (us) => {
-        let sd = new Date(start);
-        sd.setDate(sd.getDate() + 1)
-        sd.setHours(0);
+	let max = 0;
 
-        let ed = new Date(end);
-        ed.setDate(ed.getDate() + 1)
-        ed.setHours(0);
+    user.getUsers({}, (us) => {
 
         max = us.length;
 
@@ -77,20 +57,17 @@ router.get('/getAllData/:begin/:end/:interval', (req, res) => {
         us.forEach((d) => {
             let userData = [];
             urls.forEach((u) => {
-                while (sd.getTime() <= ed.getTime()) {
-                    console.log('formatting', sd, formatDate(sd));
-                    let url = `https://api.fitbit.com/1/user/${d.encodedId}/${u}/date/${formatDate(sd)}/${formatDate(sd)}/${interval}.json`;
+                let startCopy = start;
+                while (startCopy <= end) {
+					console.log("START",startCopy,"END",end);
+                    let url = `https://api.fitbit.com/1/user/${d.encodedId}/${u}/date/${formatDate(new Date(startCopy))}/${formatDate(new Date(startCopy))}/${intervalString}.json`;
+                    console.log("URL",url);
                     userData.push(api.callFitbit("GET", url, d.encodedId, d.accessToken));
-                    sd.setDate(sd.getDate() + 1);
+                    startCopy += 24 * 60 * 60 * 1000;
                 }
-                sd = new Date(start);
-                sd.setDate(sd.getDate() + 1)
-                sd.setHours(0);
-
             });
             promises.push(userData);
         });
-        console.log("PRAMPISUS", sd, promises.length, promises);
         promises.forEach((userArray) => {
             Promise.all(userArray)
                 .then((values) => {
@@ -98,13 +75,11 @@ router.get('/getAllData/:begin/:end/:interval', (req, res) => {
                     values = values.map((d) => d.vals);
                     values = [{
                         "name": id
-                              }].concat(values);
+                    }].concat(values);
 
                     resultArray.push(values);
                     fulfilled++;
-                }, (reason) => {
-                    //console.log("reason?", reason);
-                });
+                }, (reason) => {});
         });
 
     });
@@ -112,21 +87,17 @@ router.get('/getAllData/:begin/:end/:interval', (req, res) => {
     (function finisher() {
         setTimeout(() => {
             if (fulfilled === max) {
-                //console.log('IF not finished ful: ', fulfilled, " max ", max);
                 let transformed = [];
-                //console.log("RESULTS", resultArray[1][1]);
-                transformed = transformed.concat(dataTransform(resultArray[0], originalStart, originalEnd, interval));
-                for (let i = 1; i < resultArray.length; i++) {
-                    if (i % datesbtn === 0) {
-                        transformed.push((dataTransform(resultArray[i], originalStart, originalEnd, interval, false)));
-                    } else {
-                        transformed[i % datesbtn] = transformed.concat(dataTransform(resultArray[i], originalStart, originalEnd, interval, false));
-                    }
-                    console.log
+
+                let results = [];
+                let headers = generateHeaders(start, end, interval);
+                results.push(headers)
+
+                for (let i = 0; i < resultArray.length; i++) {
+                    results = results.concat(dataTransform(resultArray[i], datesbtn, results));
                 }
 
-                res.send(transformed);
-                //res.send("done!")
+                res.send(results);
 
             } else {
                 console.log('not finished ful: ', fulfilled, " max ", max);
@@ -134,15 +105,26 @@ router.get('/getAllData/:begin/:end/:interval', (req, res) => {
             }
         }, 500)
     })();
-    //	setTimeout(finisher, 0)
-    //finisher();
 });
+
+let generateHeaders = (start, end, interval) => {
+    let fields = ["ID", "Activity"];
+    let startCopy = start;
+
+    while (startCopy < end + 24 * 60 * 60 * 1000) {
+        fields.push(formatDateTime(new Date(startCopy)));
+        startCopy += interval * 60000;
+    }
+    return fields;
+};
 
 router.get('/getHeartRates', (req, res) => {
     user.getUsers({}, (us) => {
         let results = [];
         us.forEach((d) => {
-            results.push(api.callFitbit("GET", "https://api.fitbit.com/1/user/" + d.encodedId + "/activities/heart/2016-05-09.json", d.encodedId, d.accessToken));
+            results.push(api.callFitbit("GET",
+                `https://api.fitbit.com/1/user/${d.encodedId}/activities/heart/2016-05-09.json`,
+                d.encodedId, d.accessToken));
         });
         Promise.all(results)
             .then((values) => {
@@ -155,13 +137,13 @@ router.get('/getHeartRates', (req, res) => {
 
 //Gets the Authorization Code from when a user authorizes our app
 //Uses the Authorization Code to get the access and refresh tokens for the user
-router.get('/post_tokens', function (req, res) {
+router.get('/post_tokens', function(req, res) {
     let results = [];
     var url = JSON.parse(JSON.stringify(req._parsedOriginalUrl));
     var authCode = url.query.replace('code=', '');
 
     var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
+    xhr.onload = function() {
         if (this.status == 200) {
             var res = JSON.parse(this.responseText);
 
@@ -169,7 +151,7 @@ router.get('/post_tokens', function (req, res) {
             var refreshToken = res.refresh_token;
 
             var vhr = new XMLHttpRequest();
-            vhr.onload = function () {
+            vhr.onload = function() {
                 if (this.status == 200) {
                     var userObj = JSON.parse(this.responseText);
                     MongoClient.connect(dbURL, (err, db) => {
@@ -191,24 +173,22 @@ router.get('/post_tokens', function (req, res) {
     xhr.open("POST", "https://api.fitbit.com/oauth2/token");
     xhr.setRequestHeader("Authorization", "Basic " + clientEncoded);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    var params = "client_id=" + clientId + "&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fpost%5Ftokens&code=" + authCode;
+    var params = "client_id=" + clientId + "&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost:22205%2Fpost%5Ftokens&code=" + authCode;
     xhr.send(params);
 
     res.redirect("/");
 });
 
 //Not for use in final build
-router.get('/getHeart/:begin/:end', function (req, res) {
+router.get('/getHeart/:begin/:end', function(req, res) {
     var start = req.params.begin;
     var end = req.params.end;
     //var act = req.params.activity;
     var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
+    xhr.onload = function() {
         if (this.status == 200) {
             dataString = this.responseText;
             console.log(dataString);
-            //var response = JSON.parse(this.responseText);
-            //res.send(response);
         } else {
             console.log(this);
         }
@@ -219,12 +199,12 @@ router.get('/getHeart/:begin/:end', function (req, res) {
 });
 
 //Not for use in final build
-router.get('/getSteps/:begin/:end', function (req, res) {
+router.get('/getSteps/:begin/:end', function(req, res) {
     var start = req.params.begin;
     var end = req.params.end;
     //var act = req.params.activity;
     var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
+    xhr.onload = function() {
         if (this.status == 200) {
             dataString = this.responseText;
             console.log(dataString);
@@ -246,8 +226,7 @@ var padZero = (val) => val < 10 ? "0" + val : val;
 var data = [];
 
 //Takes a dateTime object and returns a format usable in the API requests
-var formatDate = function (date) {
-    console.log('DATEED', date);
+var formatDate = function(date) {
     var month = date.getMonth() + 1;
     if (month < 10) {
         month = "0" + month;
@@ -261,11 +240,11 @@ var formatDate = function (date) {
 }
 
 //Sends a specific API request to get user data
-var getActivityData = function (url, sd, ed, interval) {
+var getActivityData = function(url, sd, ed, interval) {
     var start = formatDate(sd);
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
         var xhr = new XMLHttpRequest();
-        xhr.onload = function () {
+        xhr.onload = function() {
             if (xhr.status == 200) {
                 data.push(JSON.parse(this.responseText));
                 resolve(data);
@@ -282,9 +261,9 @@ var getActivityData = function (url, sd, ed, interval) {
 //Loops through all API calls and all dates in date range
 //Adds all data to the data array
 //Transforms the data into JSON and sends it client side
-var loopUrls = function (index, sd, ed, interval, res) {
+var loopUrls = function(index, sd, ed, interval, res) {
     getActivityData(urls[index], sd, ed, interval)
-        .then(function () {
+        .then(function() {
             if (++index < urls.length) {
                 loopUrls(index, sd, ed, interval, res);
             } else {
@@ -300,7 +279,7 @@ var loopUrls = function (index, sd, ed, interval, res) {
 };
 
 //Route called to get all user data
-router.get('/getData/:begin/:end/:interval', function (req, res) {
+router.get('/getData/:begin/:end/:interval', function(req, res) {
     var start = req.params.begin;
     var end = req.params.end;
     var interval = req.params.interval;
@@ -310,11 +289,11 @@ router.get('/getData/:begin/:end/:interval', function (req, res) {
     loopUrls(0, sd, ed, interval, res);
 });
 
-router.listen(PORT, function () {
+router.listen(PORT, function() {
     console.log("Server listening on port " + PORT);
 });
 
-var formatDateTime = function (date) {
+var formatDateTime = function(date) {
     var month = date.getMonth() + 1;
     month = padZero(month);
 
@@ -336,48 +315,27 @@ var formatDateTime = function (date) {
 }
 
 //Transforms the data from an array to one JSON object
-var dataTransform = function (data, start, end, interval, headers) {
-    console.log('start and end', start, end);
-    let datesbtn = 1 + ((end.getTime() - start.getTime()) / 86400000);
-    end.setDate(end.getDate() + 1)
-    var finalData = [];
-    var fields = ["ID", "Activity"];
-
-    var inter = 0;
-    if (interval == "1min") {
-        inter = 1;
-    } else if (interval == "15min") {
-        inter = 15;
-    }
-
-    if (headers === undefined || headers === true) {
-        while (start.getTime() < end.getTime()) {
-            fields.push(formatDateTime(start));
-            start.setMinutes(start.getMinutes() + inter);
-        }
-        finalData.push(fields);
-    }
-
-    console.log("DATA", data);
+var dataTransform = function(data, datesbtwn) {
+    let rowObj = {};
+    let results = [];
+    let curRow;
+    let id = data[0].name;
     for (let i = 1; i < data.length; i++) {
-        let urlIndex = Math.floor((i - 1) / datesbtn);
-        let results = data[i][urls[urlIndex].replace('/', '-') + '-intraday'].dataset.map((d) => d.value);
-        results = [data[0].name, urls[urlIndex].replace('/', '-')].concat(results);
-        finalData.push(results);
+        let urlIndex = Math.floor((i - 1) / datesbtwn);
+		console.log("urlIndex",urlIndex);
+        let activity = urls[urlIndex].replace('/', '-');
+		console.log("data",data[i],"activity",activity);
+        if (rowObj[activity] === undefined) rowObj[activity] = [];
+        rowObj[activity] = rowObj[activity].concat(data[i][activity + '-intraday'].dataset.map((d) => d.value));
     }
 
-    let final = [finalData[0]];
-    for (let i = 1; i < finalData.length; i++) {
-        let urlIndex = Math.floor((i - 1) / datesbtn);
-        if (final[urlIndex + 1] === undefined) {
-            final[urlIndex + 1] = []
-        }else{
-			finalData[i].shift();	
-			finalData[i].shift();	
-		}
-        final[urlIndex + 1] = final[urlIndex + 1].concat(finalData[i]);
+    for (let activity of Object.keys(rowObj)) {
+        console.log('activity', activity);
+        let result = [];
+        result.push(id);
+        result.push(activity);
+        result = result.concat(rowObj[activity]);
+        results.push(result);
     }
-
-    console.log("Transform Done");
-    return final;
+    return results;
 };
